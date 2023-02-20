f3cb0167e123c44266627bb0509501fef141636f
Chanmoey <relijifa@gmail.com>
2023-02-20 17:02:37 +0800

    符合Redis协议的数字回复。

---
+++ src/main/java/com/moon/tinyredis/resp/reply/IntegerReply.java
@@ -0,0 +1,25 @@
+package com.moon.tinyredis.resp.reply;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author Chanmoey
+ * @date 2023年02月20日
+ */
+public class IntegerReply implements Reply {
+
+    private final int number;
+
+    public IntegerReply(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public byte[] toBytes() {
+        return (":" + number + Constant.CRLF).getBytes(StandardCharsets.UTF_8);
+    }
+
+    public static IntegerReply makeStatusReply(int number) {
+        return new IntegerReply(number);
+    }
+}
