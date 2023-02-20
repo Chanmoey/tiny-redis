bef6ed5c6062c3750194025b47ade376d0e1a755
Chanmoey <relijifa@gmail.com>
2023-02-20 17:06:21 +0800

    通用错误回复以及判断是否是异常回复。

---
+++ src/main/java/com/moon/tinyredis/resp/reply/StandardErrorReply.java
@@ -0,0 +1,25 @@
+package com.moon.tinyredis.resp.reply;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author Chanmoey
+ * @date 2023年02月20日
+ */
+public class StandardErrorReply implements Reply {
+
+    private final String status;
+
+    public StandardErrorReply(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public byte[] toBytes() {
+        return ("-" + status + Constant.CRLF).getBytes(StandardCharsets.UTF_8);
+    }
+
+    public static StandardErrorReply makeStatusReply(String status) {
+        return new StandardErrorReply(status);
+    }
+}
