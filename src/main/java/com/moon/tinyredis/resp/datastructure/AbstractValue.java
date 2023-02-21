242677546acf0aa31ca873b3220de80fc05e28c2
Chanmoey <relijifa@gmail.com>
2023-02-21 19:55:11 +0800

    定义存储在TinyRedis中的数据。

---
+++ src/main/java/com/moon/tinyredis/resp/datastructure/AbstractValue.java
@@ -0,0 +1,17 @@
+package com.moon.tinyredis.resp.datastructure;
+
+/**
+ * @author Chanmoey
+ * @date 2023年02月21日
+ */
+public class AbstractValue {
+
+    protected long expireTime = -1;
+
+    protected boolean isExpire() {
+        if (expireTime == -1L) {
+            return false;
+        }
+        return expireTime < System.currentTimeMillis();
+    }
+}
---
+++ src/main/java/com/moon/tinyredis/resp/datastructure/Value.java
@@ -0,0 +1,10 @@
+package com.moon.tinyredis.resp.datastructure;
+
+/**
+ * 所有存储在TinyRedis中的value，都必须继承这个接口
+ *
+ * @author Chanmoey
+ * @date 2023年02月21日
+ */
+public interface Value {
+}
