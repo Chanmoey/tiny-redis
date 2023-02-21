61d1eac4ccd6d3cedf9e50445bfec8af8124d9e1
Chanmoey <relijifa@gmail.com>
2023-02-21 21:35:41 +0800

    定义Dick接口。

---
+++ src/main/java/com/moon/tinyredis/resp/datastructure/dick/Dick.java
@@ -0,0 +1,34 @@
+package com.moon.tinyredis.resp.datastructure.dick;
+
+import com.moon.tinyredis.resp.datastructure.value.Value;
+
+import java.util.function.BiConsumer;
+
+/**
+ * @author Chanmoey
+ * @date 2023年02月21日
+ */
+public interface Dick {
+
+    Value get(String key);
+
+    int len();
+
+    int put(String key, Value value);
+
+    int putIfAbsent(String key, Value value);
+
+    int putIfExists(String key, Value value);
+
+    int remove(String key);
+
+    void forEach(BiConsumer<String, Value> action);
+
+    String[] keys(String regex);
+
+    String[] randomKeys(int limit);
+
+    String[] randomDistinctKeys(int limit);
+
+    void clear();
+}
