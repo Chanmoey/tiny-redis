f8fbeeccfde6dd24dbbea34b9b7e5332eba58741
Chanmoey <relijifa@gmail.com>
2023-02-27 01:30:17 +0800

    传输消息构造器。

--- src/main/java/com/moon/tinyredis/resp/parser/Message.java
+++ src/main/java/com/moon/tinyredis/resp/parser/Message.java
@@ -1,8 +1,12 @@
 package com.moon.tinyredis.resp.parser;
 
 import com.moon.tinyredis.resp.reply.Reply;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 
 /**
+ * 用来构建网络传输消息数据
+ *
  * @author Chanmoey
  * @date 2023年02月26日
  */
@@ -10,7 +14,10 @@ public class Message {
 
     public static final int BODY_LENGTH = 4;
 
-    private int bodyLength;
-
-    private Reply reply;
+    public static ByteBuf makeMessage(Reply reply) {
+        ByteBuf buf = ByteBufAllocator.DEFAULT.buffer(reply.toBytes().length + BODY_LENGTH);
+        buf.writeInt(reply.toBytes().length);
+        buf.writeBytes(reply.toBytes());
+        return buf;
+    }
 }
