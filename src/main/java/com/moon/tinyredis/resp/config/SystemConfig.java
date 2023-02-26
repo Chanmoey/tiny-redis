73f3dc55d4a2e00c35cd387c9d53f15249757efe
Chanmoey <relijifa@gmail.com>
2023-02-26 22:13:55 +0800

    统一管理项目的字符集。

---
+++ src/main/java/com/moon/tinyredis/resp/config/SystemConfig.java
@@ -0,0 +1,13 @@
+package com.moon.tinyredis.resp.config;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author Chanmoey
+ * @date 2023年02月26日
+ */
+public class SystemConfig {
+    
+    private static final Charset SYSTEM_CHARSET = SystemConfig.SYSTEM_CHARSET;
+}
