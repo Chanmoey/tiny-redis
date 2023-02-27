fe6af776defcde17a42fb1e01303c963fff48115
Chanmoey <relijifa@gmail.com>
2023-02-27 09:52:16 +0800

    创建连接管理器，管理客户端的连接。

--- src/main/java/com/moon/tinyredis/resp/handler/CommandHandler.java
+++ src/main/java/com/moon/tinyredis/resp/handler/CommandHandler.java
@@ -9,6 +9,9 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 /**
  * 对指令进行转发，如果是正常的指令，则交由Database进行执行处理
  * 如果是错误的指令，则回送错误信息
@@ -17,6 +20,9 @@ import io.netty.channel.SimpleChannelInboundHandler;
  * @date 2023年02月27日
  */
 public class CommandHandler extends SimpleChannelInboundHandler<PayLoad> {
+
+    private static final Executor executor = Executors.newSingleThreadExecutor();
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, PayLoad msg) throws Exception {
         if (msg.getException() != null) {
@@ -26,6 +32,7 @@ public class CommandHandler extends SimpleChannelInboundHandler<PayLoad> {
             return;
         }
 
+        // 通过线程池将IO与业务处理进行解耦，并将指令转发给Database进行处理
         System.out.println(new String(msg.getData().toBytes(), SystemConfig.SYSTEM_CHARSET));
     }
 }
